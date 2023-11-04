d2cc20bcef90fb3906dabf8ef8f07a19c245f482
shivam gurunani <namisha.gurunani173261@gmail.com>
2023-11-04 23:46:13 +0530

    project done

---
+++ Interactive-Data-Visualization/script.js
@@ -0,0 +1,47 @@
+// Chart.defaults.font.size=20
+const ctx = document.getElementById('myChart');
+        new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ['January', 'February', 'March', 'April', 'May'],
+            datasets: [{
+              label: 'Monthly Sales',
+              data: [65, 59, 80, 81, 56],
+              borderWidth: 1,
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)'
+              ],
+            }]
+          },
+          options: {
+            plugins:{
+                tooltips: {
+                    titleFontSize:16,
+                    bodyFontSize:14,
+                    footerFontSize:12
+                },
+                title: {
+                    display:true,
+                    text:"Custom Chart Title",
+                    padding: {
+                        top: 10,
+                        bottom: 20
+                    },
+                },
+                legend: {
+                    labels: {
+                        font: {
+                            size: 16
+                        }
+                    }
+                }
+            },
+            onClick: function(){
+                alert("You clicked on a bar!");
+            }
+          },
+        });
