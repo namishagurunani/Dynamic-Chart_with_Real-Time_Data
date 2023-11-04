e2509c6dd81f6a2655bb7e29d4be175356ec9e2f
shivam gurunani <namisha.gurunani173261@gmail.com>
2023-11-05 00:33:59 +0530

    project done

--- Interactive-Data-Visualization/script.js
+++ Interactive-Data-Visualization/script.js
@@ -8,12 +8,13 @@ const ctx = document.getElementById('myChart');
               label: 'Monthly Sales',
               data: [65, 59, 80, 81, 56],
               borderWidth: 1,
+              
               backgroundColor: [
                 'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
+                'rgb(54, 162, 235)',
                 'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)'
+                'rgb(195, 123, 756)',
+                'rgb(256, 105, 56)'
               ],
             }]
           },
